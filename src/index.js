e0cc8f3911cde8ce32a46e7c18515de83d881d53
Oluwafemi Awoyemi <femikola23@gmail.com>
2022-06-04 10:35:56 +0400

    make better

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/redux/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -20,8 +19,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
